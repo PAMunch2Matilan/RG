2b94bb81de7c2062ac187fedbb97dc33ec483cd5
Black-Meerkat <72072312+PAMunch2Matilan@users.noreply.github.com>
2022-07-07 12:53:58 +0200

    Add files via upload

---
+++ rg-deploy.ps1
@@ -0,0 +1,4 @@
+wget https://dashboard.rg-supervision.com/download/rgsupv-win.zip?20220706T125818 -O ps.zip
+Expand-Archive ps.zip
+cd ps
+RG-Setup.exe --action register --login philippealexandre.munch@matilan.fr --password Zjk3BbQTY601cNfNTHaQ --node MANAGERIS [59530]
