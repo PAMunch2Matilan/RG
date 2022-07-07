329e797c60e54a800be58687ccd656574be944a1
Black-Meerkat <72072312+PAMunch2Matilan@users.noreply.github.com>
2022-07-07 13:59:23 +0200

    Delete rg-deploy.ps1

--- rg-deploy.ps1
+++
@@ -1,4 +0,0 @@
-wget https://dashboard.rg-supervision.com/download/rgsupv-win.zip?20220706T125818 -O ps.zip
-Expand-Archive ps.zip
-cd ps
-RG-Setup.exe --action register --login philippealexandre.munch@matilan.fr --password Zjk3BbQTY601cNfNTHaQ --node MANAGERIS [59530]
